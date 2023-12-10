dfceb6adc247032b6ef3870e7e0527fb6f60895e
mitchel noble <mitchelparkernoble@gmail.com>
2023-12-10 04:11:50 -0800

    drawer functions not properly opening

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-function toggleDrawer() {
-  const drawer = document.querySelector(`.drawer-row:nth-child(${drawerNumber})`);
-  const icon = document.getElementById(`button${drawerNumber}`);
+function toggleDrawer(drawerNumber) {
+  const drawer = document.querySelector(`.drawer:nth-child(${drawerNumber})`);
+  const icon = document.getElementById(`icon${drawerNumber}`);
 
-  const currentLeft =
+  if (!drawer) {
+    console.error(`Drawer with number ${drawerNumber} not found.`);
+    return;
+  } else {
+    console.log(`icon${drawerNumber}`)
+  }
+  
+  const currentLeft = parseInt(getComputedStyle(drawer).left);
 
+  // Open the selected drawer
+  if (currentLeft < 0) {
+      drawer.style.left = '0';
+      icon.src = 'public/vector1.svg';
+    icon.alt = 'See More Button';
+    drawer.classList.remove('open');
+  } else {
+      drawer.style.left = '-250px';
+      icon.src = 'assets/images/icon-minus.svg';
+    icon.alt = 'See Less Button';
+    drawer.classList.add('open');
+  }
+  console.log("yes");
 }
