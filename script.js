e81e67336a89a0b7ef5a83b3b71ef26ed2f6c2e9
mitchel noble <mitchelparkernoble@gmail.com>
2023-12-11 16:14:22 -0800

    working drawer function

--- script.js
+++ script.js
@@ -1,28 +1,35 @@
-function toggleDrawer(button) {
-  const drawerNumber = button.getAttribute('data-drawer-icon');
-  const drawer = document.querySelector(`.drawer:nth-child(${drawerNumber})`);
-  const icon = document.getElementById(`icon${drawerNumber}`);
+// function toggleDrawer(button) {
+//   const drawerNumber = button.getAttribute('data-drawer-icon');
+//   const drawer = document.querySelector(`.drawer:nth-child(${drawerNumber})`);
+//   const icon = document.getElementById(`icon${drawerNumber}`);
 
-  if (!drawer) {
-    console.error(`Drawer with number ${drawerNumber} not found.`);
-    return;
-  } else {
-    console.log(icon)
-  }
+//   if (!drawer) {
+//     console.error(`Drawer with number ${drawerNumber} not found.`);
+//     return;
+//   } else {
+//     console.log(icon)
+//   }
 
-  const currentLeft = parseInt(getComputedStyle(drawer).left);
+//   const currentLeft = parseInt(getComputedStyle(drawer).left);
 
-  // Open the selected drawer
-  if (currentLeft < 0) {
-      drawer.style.left = '0';
-      icon.src = 'public/vector1.svg';
-    icon.alt = 'See More Button';
-    drawer.classList.remove('open');
-  } else {
-      drawer.style.left = '-250px';
-      icon.src = 'assets/images/icon-minus.svg';
-    icon.alt = 'See Less Button';
-    drawer.classList.add('open');
-  }
-  console.log("yes");
+
+//   if (currentLeft < 0) {
+//       drawer.style.left = '0';
+//       icon.src = 'public/vector1.svg';
+//     icon.alt = 'See More Button';
+//     drawer.classList.remove('open');
+//   } else {
+//       drawer.style.left = '-250px';
+//       icon.src = 'assets/images/icon-minus.svg';
+//     icon.alt = 'See Less Button';
+//     drawer.classList.add('open');
+//   }
+//   console.log("yes");
+// }
+
+function toggleDrawer(drawerId) {
+  var drawer = document.getElementById(drawerId);
+  var content = drawer.querySelector('.drawer-content');
+
+  content.classList.toggle('active');
 }
